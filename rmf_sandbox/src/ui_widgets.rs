115f3a36546b5f1392b83309c291ef6a23551a05
Morgan Quigley <morgan@osrfoundation.org>
2022-05-09 22:08:37 +0800

    WIP asset loading and caching
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- rmf_sandbox/src/ui_widgets.rs
+++ rmf_sandbox/src/ui_widgets.rs
@@ -2,7 +2,7 @@ use super::camera_controls::{CameraControls, ProjectionMode};
 use super::level::Level;
 use super::site_map::{Handles, SpawnSiteMapYaml};
 use super::vertex::Vertex;
-use super::wall::Wall;
+//use super::wall::Wall;
 use bevy::{
     app::AppExit,
     prelude::*,
