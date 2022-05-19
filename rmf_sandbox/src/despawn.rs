b348e072c26a82234349711c8de98912a8f92a60
Teo Koon Peng <koonpeng@openrobotics.org>
2022-05-19 11:18:00 +0800

    improve despawner so that callers dont need to call tick every frame
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- rmf_sandbox/src/despawn.rs
+++ rmf_sandbox/src/despawn.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use bevy::{ecs::system::SystemParam, prelude::*};
 
@@ -17,26 +17,40 @@ pub struct DespawnBlocker();
 /// Sent when an entity has despawned.
 pub struct Despawned(pub Entity);
 
+#[derive(Default)]
+pub struct HandleCounter(usize);
+
+#[derive(Default)]
+pub struct DespawnTracker(HashMap<usize, HashSet<Entity>>);
+
+/// A wrapper over the `Despawn` event that tracks `Despawned` events to track when
+/// the entities have all been despawned.
+/// 
+/// The `Despawned` event is being tracked in the `PreUpdate` stage to avoid frame
+/// delays as much as possible. Do note that there may be a 1 frame delay if your
+/// system also runs in the `PreUpdate` stage.
 #[derive(SystemParam)]
-pub struct DespawnTracker<'w, 's> {
+pub struct Despawner<'w, 's> {
     despawn_writer: EventWriter<'w, 's, Despawn>,
-    despawned_reader: EventReader<'w, 's, Despawned>,
-    pub pending: Local<'s, HashSet<Entity>>,
+    handle_counter: ResMut<'w, HandleCounter>,
+    tracker: ResMut<'w, DespawnTracker>,
 }
 
-impl<'w, 's> DespawnTracker<'w, 's> {
-    /// This MUST be called every frame to ensure that despawned events are not
-    /// missed.
-    /// TODO: Anyway to auto call this every frame?
-    pub fn tick(&mut self) {
-        for e in self.despawned_reader.iter() {
-            self.pending.remove(&e.0);
+impl<'w, 's> Despawner<'w, 's> {
+    pub fn despawn<I: IntoIterator<Item = Entity>>(&mut self, entities: I) -> usize {
+        let handle = self.handle_counter.0;
+        self.handle_counter.0 += 1;
+        self.tracker.0.insert(handle, HashSet::new());
+        let pending = self.tracker.0.get_mut(&handle).unwrap();
+        for e in entities {
+            pending.insert(e);
+            self.despawn_writer.send(Despawn(e));
         }
+        handle
     }
 
-    pub fn despawn(&mut self, entity: Entity) {
-        self.despawn_writer.send(Despawn(entity));
-        self.pending.insert(entity);
+    pub fn pending(&self, handle: usize) -> bool {
+        self.tracker.0.contains_key(&handle)
     }
 }
 
@@ -66,12 +80,27 @@ fn despawn_system(
     }
 }
 
+fn despawn_tracker_system(
+    mut tracker: ResMut<DespawnTracker>,
+    mut despawned: EventReader<Despawned>,
+) {
+    for e in despawned.iter() {
+        for entities in tracker.0.values_mut() {
+            entities.remove(&e.0);
+        }
+    }
+    tracker.0.retain(|_, v| v.len() > 0);
+}
+
 pub struct DespawnPlugin;
 
 impl Plugin for DespawnPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<Despawn>()
             .add_event::<Despawned>()
+            .init_resource::<HandleCounter>()
+            .init_resource::<DespawnTracker>()
+            .add_system_to_stage(CoreStage::PreUpdate, despawn_tracker_system)
             .add_system_to_stage(CoreStage::PostUpdate, despawn_system);
     }
 }
