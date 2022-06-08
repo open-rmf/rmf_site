89f19c03f5cc9ca0cc7c0a8369a75303e1728351
Teo Koon Peng <koonpeng@openrobotics.org>
2022-06-08 10:17:28 +0800

    fix picking blocker again; fix door type conversion
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- rmf_sandbox/src/door.rs
+++ rmf_sandbox/src/door.rs
@@ -20,23 +20,61 @@ pub struct DoorProperties {
 pub struct Door(pub usize, pub usize, pub DoorProperties);
 
 pub enum DoorType {
-    SingleSliding = 1,
-    DoubleSliding = 2,
-    SingleTelescope = 3,
-    DoubleTelescope = 4,
-    SingleSwing = 5,
-    DoubleSwing = 6,
+    Unknown,
+    SingleSliding,
+    DoubleSliding,
+    SingleTelescope,
+    DoubleTelescope,
+    SingleSwing,
+    DoubleSwing,
+    SingleHinged,
+    DoubleHinged,
+}
+
+impl DoorType {
+    pub fn to_value(&self) -> String {
+        match self {
+            DoorType::Unknown => "unknown".to_string(),
+            DoorType::SingleSliding => "sliding".to_string(),
+            DoorType::DoubleSliding => "double_sliding".to_string(),
+            DoorType::SingleTelescope => "telescope".to_string(),
+            DoorType::DoubleTelescope => "double_telescope".to_string(),
+            DoorType::SingleSwing => "swing".to_string(),
+            DoorType::DoubleSwing => "double_swing".to_string(),
+            DoorType::SingleHinged => "hinged".to_string(),
+            DoorType::DoubleHinged => "double_hinged".to_string(),
+        }
+    }
+}
+
+impl From<&str> for DoorType {
+    fn from(s: &str) -> Self {
+        match s {
+            "sliding" => DoorType::SingleSliding,
+            "double_sliding" => DoorType::DoubleSliding,
+            "telescope" => DoorType::SingleTelescope,
+            "double_telescope" => DoorType::DoubleTelescope,
+            "swing" => DoorType::SingleSwing,
+            "double_swing" => DoorType::DoubleSwing,
+            "hinged" => DoorType::SingleHinged,
+            "double_hinged" => DoorType::DoubleHinged,
+            _ => DoorType::Unknown,
+        }
+    }
 }
 
 impl Display for DoorType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            DoorType::SingleSliding => write!(f, "Single Sliding"),
+            DoorType::Unknown => write!(f, "Unknown"),
+            DoorType::SingleSliding => write!(f, "Sliding"),
             DoorType::DoubleSliding => write!(f, "Double Sliding"),
-            DoorType::SingleTelescope => write!(f, "Single Telescope"),
+            DoorType::SingleTelescope => write!(f, "Telescope"),
             DoorType::DoubleTelescope => write!(f, "Double Telescope"),
-            DoorType::SingleSwing => write!(f, "Single Swing"),
+            DoorType::SingleSwing => write!(f, "Swing"),
             DoorType::DoubleSwing => write!(f, "Double Swing"),
+            DoorType::SingleHinged => write!(f, "Hinged"),
+            DoorType::DoubleHinged => write!(f, "Double Hinged"),
         }
     }
 }
@@ -48,4 +86,6 @@ pub static DOOR_TYPES: &[DoorType] = &[
     DoorType::DoubleTelescope,
     DoorType::SingleSwing,
     DoorType::DoubleSwing,
+    DoorType::SingleHinged,
+    DoorType::DoubleHinged,
 ];
