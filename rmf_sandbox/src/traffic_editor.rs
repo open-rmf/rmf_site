147811be9b84814cb13fd934ef15f7df97552163
Luca Della Vedova <luca@openrobotics.org>
2022-07-14 16:02:32 +0800

    Style
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- rmf_sandbox/src/traffic_editor.rs
+++ rmf_sandbox/src/traffic_editor.rs
@@ -769,7 +769,9 @@ fn enable_picking(
     // Normally bevy_mod_picking automatically stops when
     // a bevy ui node is in focus, but bevy_egui does not use bevy ui node.
     let egui_ctx = egui_context.ctx_mut();
-    let enable = !egui_ctx.wants_pointer_input() && !egui_ctx.wants_keyboard_input() && !egui_ctx.is_pointer_over_area();
+    let enable = !egui_ctx.wants_pointer_input()
+        && !egui_ctx.wants_keyboard_input()
+        && !egui_ctx.is_pointer_over_area();
 
     let mut blocker = picking_blocker.single_mut();
     if enable {
