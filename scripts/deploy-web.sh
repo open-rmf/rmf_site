ee28185768e0cac3bbd9b1e8a1ac5a5607687a81
Grey <grey@openrobotics.org>
2022-10-28 15:14:51 +0800

    Fix deploy script (#69)
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -19,16 +19,15 @@ echo "Starting deployment process using branch $BRANCH"
 
 set -o verbose
 
-git clone ssh://git@github.com/open-rmf/rmf_site_editor temp-deploy-checkout --branch $BRANCH --single-branch --depth 1
+git clone ssh://git@github.com/open-rmf/rmf_site temp-deploy-checkout --branch $BRANCH --single-branch --depth 1
 
-cd temp-deploy-checkout/rmf_editor
+cd temp-deploy-checkout
 git checkout --orphan gh-pages
 git reset
 scripts/build-web.sh
 
 git add -f web
-cd ..
-cp rmf_editor/web/root_index.html index.html
+cp web/root_index.html index.html
 git add index.html
 
 git commit -a -m "publish to github pages"
