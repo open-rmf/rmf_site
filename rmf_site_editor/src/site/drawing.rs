591469dd76e10ef57673ee9c4029630244ad9d2c
Luca Della Vedova <luca@openrobotics.org>
2022-11-18 17:49:13 +0800

    Fix compile error
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- rmf_site_editor/src/site/drawing.rs
+++ rmf_site_editor/src/site/drawing.rs
@@ -38,7 +38,7 @@ pub fn add_drawing_visuals(
     site_files: Query<&DefaultFile>,
 ) {
     // TODO support for remote sources
-    let file_path = match get_current_site_path(current_site, site_files).unwrap_or(PathBuf::new());
+    let file_path = match get_current_site_path(current_site, site_files) {
         Some(file_path) => file_path,
         None => return,
     };
