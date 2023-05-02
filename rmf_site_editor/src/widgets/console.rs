41eee1bcc24dda5981e40e4734fd8d1af3bb9a56
Xi Yu Oh <xiyuoh@intrinsic.ai>
2023-05-02 13:28:56 +0800

    Add formatting to printouts
    Signed-off-by: Xi Yu Oh <xiyuoh@intrinsic.ai>

--- rmf_site_editor/src/widgets/console.rs
+++ rmf_site_editor/src/widgets/console.rs
@@ -89,9 +89,11 @@ impl Default for LogHistory {
         let filter_layer = EnvFilter::try_from_default_env()
             .or_else(|_| EnvFilter::try_new(&default_filter))
             .unwrap();
+        let fmt_layer = tracing_subscriber::fmt::Layer::default();
 
         let subscriber = tracing_subscriber::registry().with(LogSubscriber { sender: tx_2 });
         let subscriber = subscriber.with(filter_layer);
+        let subscriber = subscriber.with(fmt_layer);
         tracing::subscriber::set_global_default(subscriber);
 
         Self {
