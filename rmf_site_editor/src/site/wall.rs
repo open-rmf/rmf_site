40c773544fc888041e4545c9d4dfe76d0ba8b575
Luca Della Vedova <lucadv@intrinsic.ai>
2023-06-08 11:37:18 +0800

    Fix wall material disappearing while editing texture
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>

--- rmf_site_editor/src/site/wall.rs
+++ rmf_site_editor/src/site/wall.rs
@@ -135,7 +135,7 @@ pub fn update_wall_for_changed_texture(
             &Edge<Entity>,
             &Texture,
             &mut Handle<Mesh>,
-            &mut Handle<StandardMaterial>,
+            &Handle<StandardMaterial>,
         ),
         (Changed<Texture>, With<WallMarker>),
     >,
@@ -144,18 +144,17 @@ pub fn update_wall_for_changed_texture(
     anchors: AnchorParams,
     asset_server: Res<AssetServer>,
 ) {
-    for (e, edge, texture, mut mesh, mut material) in &mut changed_walls {
+    for (e, edge, texture, mut mesh, material) in &mut changed_walls {
         *mesh = meshes.add(make_wall(e, edge, texture, &anchors));
-        let (base_color, alpha_mode) = if let Some(alpha) = texture.alpha.filter(|a| a < &1.0) {
-            (*Color::default().set_a(alpha), AlphaMode::Blend)
-        } else {
-            (Color::default(), AlphaMode::Opaque)
-        };
-        *material = materials.add(StandardMaterial {
-            base_color_texture: Some(asset_server.load(&String::from(&texture.source))),
-            base_color,
-            alpha_mode,
-            ..default()
-        });
+        if let Some(mut material) = materials.get_mut(material) {
+            let (base_color, alpha_mode) = if let Some(alpha) = texture.alpha.filter(|a| a < &1.0) {
+                (*Color::default().set_a(alpha), AlphaMode::Blend)
+            } else {
+                (Color::default(), AlphaMode::Opaque)
+            };
+            material.base_color_texture = Some(asset_server.load(&String::from(&texture.source)));
+            material.base_color = base_color;
+            material.alpha_mode = alpha_mode;
+        }
     }
 }
