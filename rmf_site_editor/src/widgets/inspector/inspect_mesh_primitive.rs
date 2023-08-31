25c74fe6e13f0385408eba570ab350f6935d307c
Luca Della Vedova <lucadv@intrinsic.ai>
2023-08-31 14:23:12 +0800

    Make mesh primitive inspector read only
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>

--- rmf_site_editor/src/widgets/inspector/inspect_mesh_primitive.rs
+++ rmf_site_editor/src/widgets/inspector/inspect_mesh_primitive.rs
@@ -31,46 +31,35 @@ impl<'a> InspectMeshPrimitive<'a> {
     pub fn show(self, ui: &mut Ui) -> Option<MeshPrimitive> {
         let mut new_primitive = self.primitive.clone();
         ui.horizontal(|ui| {
-            ui.label("Primitive");
-            ComboBox::from_id_source("Mesh Primitive")
-                .selected_text(new_primitive.label())
-                .show_ui(ui, |ui| {
-                    for variant in &[
-                        self.recall.assume_box(self.primitive),
-                        self.recall.assume_cylinder(self.primitive),
-                        self.recall.assume_capsule(self.primitive),
-                        self.recall.assume_sphere(self.primitive),
-                    ] {
-                        ui.selectable_value(&mut new_primitive, variant.clone(), variant.label());
-                    }
-                    ui.end_row();
-                });
+            ui.label("Primitive:");
+            // TODO(luca) make this a ComboBox again once we have a system to update primitives
+            ui.label(new_primitive.label());
         });
-        // TODO(luca) Create systems to update meshes when the values are changed
+        // TODO(luca) Make these values editable and implement a system to parse changes
         match &mut new_primitive {
             MeshPrimitive::Box { size } => {
                 ui.label("Size");
                 ui.horizontal(|ui| {
-                    ui.label("X");
-                    ui.add(DragValue::new(&mut size[0]).clamp_range(0_f32..=std::f32::INFINITY));
-                    ui.label("Y");
-                    ui.add(DragValue::new(&mut size[1]).clamp_range(0_f32..=std::f32::INFINITY));
-                    ui.label("Z");
-                    ui.add(DragValue::new(&mut size[2]).clamp_range(0_f32..=std::f32::INFINITY));
+                    ui.label("X:");
+                    ui.label(size[0].to_string());
+                    ui.label("Y:");
+                    ui.label(size[1].to_string());
+                    ui.label("Z:");
+                    ui.label(size[2].to_string());
                 });
             }
             MeshPrimitive::Cylinder { radius, length } | MeshPrimitive::Capsule { radius, length }=> {
                 ui.horizontal(|ui| {
-                    ui.label("Radius");
-                    ui.add(DragValue::new(radius).clamp_range(0_f32..=std::f32::INFINITY));
-                    ui.label("Length");
-                    ui.add(DragValue::new(length).clamp_range(0_f32..=std::f32::INFINITY));
+                    ui.label("Radius:");
+                    ui.label(radius.to_string());
+                    ui.label("Length:");
+                    ui.label(length.to_string());
                 });
             }
             MeshPrimitive::Sphere { radius } => {
                 ui.horizontal(|ui| {
-                    ui.label("Radius");
-                    ui.add(DragValue::new(radius).clamp_range(0_f32..=std::f32::INFINITY));
+                    ui.label("Radius:");
+                    ui.label(radius.to_string());
                 });
             }
         }
