75f5694c544aed1385aebd5475ddb34e39d101ba
Audrow Nash <audrow@intrinsic.ai>
2023-10-09 14:07:29 +0000

    Generate a package from a workcell
    Signed-off-by: Audrow Nash <audrow@intrinsic.ai>

--- demo_package_exporter/src/main.rs
+++ demo_package_exporter/src/main.rs
@@ -1,3 +1,4 @@
+use rmf_site_format::Workcell;
 use package_exporter::{generate_package,PackageContext, Person, generate_package_2};
 
 fn generate_package_1() {
@@ -28,5 +29,24 @@ fn generate_package_1() {
 
 }
 fn main() {
-    generate_package_2();
+    let path_to_workcell = "/usr/local/google/home/audrow/Documents/nexus/rmf_site/test-local.workcell.json";
+    let output_directory = "output".to_string();
+
+    let workcell_contents = std::fs::read_to_string(path_to_workcell).unwrap();
+    let workcell = Workcell::from_str(&workcell_contents).unwrap();
+
+    let package_context = PackageContext {
+        project_name: "output_package".to_string(),
+        project_version: "1.0.0".to_string(),
+        project_description: "A generated package from an rmf_site workcell".to_string(),
+        license: "MIT".to_string(),
+        maintainers: vec![Person {
+            name: "Audrow Nash".to_string(),
+            email: "audrow@intrinsic.ai".to_string(),
+        }],
+        dependencies: vec![],
+        fixed_frame: "base_link".to_string(),
+        urdf_file_name: "robot.urdf".to_string(),
+    };
+    generate_package_2(&workcell, &package_context, &output_directory);
 }
