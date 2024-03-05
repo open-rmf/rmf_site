c211312ed88820c4afe51a901738ed235660f245
Luca Della Vedova <lucadv@intrinsic.ai>
2024-03-05 17:16:01 +0800

    Add automatic lift floor hole generation (#196)
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>
    Co-authored-by: Michael X. Grey <grey@openrobotics.org>

--- rmf_site_editor/src/site/floor.rs
+++ rmf_site_editor/src/site/floor.rs
@@ -21,10 +21,9 @@ use bevy::{
     prelude::*,
     render::mesh::{Indices, PrimitiveTopology},
 };
-use lyon::{
-    math::point,
-    path::Path as LyonPath,
-    tessellation::{geometry_builder::simple_builder, *},
+use geo::{
+    geometry::{LineString, MultiPolygon, Polygon},
+    BooleanOps, CoordsIter, TriangulateSpade,
 };
 use rmf_site_format::{FloorMarker, Path, Texture};
 
@@ -70,6 +69,7 @@ fn make_floor_mesh(
     anchor_path: &Path<Entity>,
     texture: &Texture,
     anchors: &AnchorParams,
+    lifts: &Query<(&Transform, &LiftCabin<Entity>)>,
 ) -> Mesh {
     if anchor_path.len() == 0 {
         return Mesh::new(PrimitiveTopology::TriangleList);
@@ -99,71 +99,83 @@ fn make_floor_mesh(
             .into();
     }
 
-    let mut builder = LyonPath::builder();
-    let mut first = true;
-    let mut valid = true;
     let mut reference_positions = Vec::new();
+    let mut valid = true;
     for anchor in &anchor_path.0 {
-        let p = match anchors.point_in_parent_frame_of(*anchor, Category::Floor, entity) {
-            Ok(a) => a,
+        match anchors.point_in_parent_frame_of(*anchor, Category::Floor, entity) {
+            Ok(p) => reference_positions.push(p.to_array()),
             Err(_) => {
                 error!("Failed to find anchor {anchor:?} used by a path");
                 valid = false;
                 continue;
             }
         };
-
-        reference_positions.push(p.to_array());
-        if first {
-            first = false;
-            builder.begin(point(p.x, p.y));
-        } else {
-            builder.line_to(point(p.x, p.y));
-        }
     }
+    let mut polygon = MultiPolygon::from(Polygon::new(
+        LineString::from(
+            reference_positions
+                .iter()
+                .map(|p| [p[0], p[1]])
+                .collect::<Vec<_>>(),
+        ),
+        vec![],
+    ));
     let outline_buffer = make_closed_path_outline(reference_positions);
 
     if !valid {
         return make_fallback_floor_mesh_near_path(entity, anchor_path, anchors);
     }
-
-    builder.close();
-    let path = builder.build();
-
-    let mut buffers = VertexBuffers::new();
-    {
-        let mut vertex_builder = simple_builder(&mut buffers);
-        let mut tessellator = FillTessellator::new();
-        let result = tessellator.tessellate_path(
-            path.as_slice(),
-            &FillOptions::default(),
-            &mut vertex_builder,
-        );
-
-        match result {
-            Err(err) => {
-                error!("Failed to render floor: {err}");
-                return make_fallback_floor_mesh_near_path(entity, anchor_path, anchors);
-            }
-            _ => {}
+    // Subtract all the lift cabin AABBs
+    for (tf, cabin) in lifts.iter() {
+        let to_subtract = match cabin {
+            LiftCabin::Rect(params) => {
+                let w = params.thickness();
+                let gap_for_door = |d: &Option<LiftCabinDoorPlacement<Entity>>| -> f32 {
+                    d.and_then(|d| d.custom_gap).unwrap_or(params.gap()) + w
+                };
+                let aabb = params.aabb();
+                let tf_cabin = *tf * Transform::from_translation(aabb.center.into());
+                let front_gap = gap_for_door(&params.front_door);
+                let right_gap = gap_for_door(&params.right_door);
+                let back_gap = gap_for_door(&params.back_door);
+                let left_gap = gap_for_door(&params.left_door);
+                let he = aabb.half_extents;
+                let he0 = Vec3::new(he.x + front_gap, he.y + left_gap, 0.0);
+                let he1 = Vec3::new(he.x + front_gap, -he.y - right_gap, 0.0);
+                let he2 = Vec3::new(-he.x - back_gap, -he.y - right_gap, 0.0);
+                let he3 = Vec3::new(-he.x - back_gap, he.y + left_gap, 0.0);
+                let p0 = tf_cabin.transform_point(he0);
+                let p1 = tf_cabin.transform_point(he1);
+                let p2 = tf_cabin.transform_point(he2);
+                let p3 = tf_cabin.transform_point(he3);
+                Polygon::new(
+                    LineString::from(vec![[p0.x, p0.y], [p1.x, p1.y], [p2.x, p2.y], [p3.x, p3.y]]),
+                    vec![],
+                )
+            } // When new lift types are added, add their footprint calculation here.
+        };
+        polygon = polygon.difference(&to_subtract.into());
+    }
+    let mut positions: Vec<[f32; 3]> = Vec::new();
+    for polygon in polygon.iter() {
+        let Ok(triangles) = polygon.constrained_triangulation(Default::default()) else {
+            warn!("Failed triangulating lift floor hole");
+            continue;
+        };
+        positions.reserve(triangles.len() * 3);
+        for triangle in triangles.iter() {
+            positions.extend(triangle.coords_iter().map(|v| [v.x, v.y, 0.]));
         }
     }
 
     let texture_width = texture.width.unwrap_or(1.0);
     let texture_height = texture.height.unwrap_or(1.0);
-    let positions: Vec<[f32; 3]> = buffers.vertices.iter().map(|v| [v.x, v.y, 0.]).collect();
-    let normals: Vec<[f32; 3]> = buffers.vertices.iter().map(|_| [0., 0., 1.]).collect();
-    let uv: Vec<[f32; 2]> = buffers
-        .vertices
+    let indices = (0..positions.len() as u32).collect();
+    let normals: Vec<[f32; 3]> = positions.iter().map(|_| [0., 0., 1.]).collect();
+    let uv: Vec<[f32; 2]> = positions
         .iter()
-        .map(|v| [v.x / texture_width, v.y / texture_height])
+        .map(|v| [v[0] / texture_width, v[1] / texture_height])
         .collect();
-    for i in 0..buffers.indices.len() / 3 {
-        let i1 = 3 * i + 1;
-        let i2 = 3 * i + 2;
-        buffers.indices.swap(i1, i2);
-    }
-    let indices = buffers.indices.drain(..).map(|v| v as u32).collect();
 
     MeshBuffer::new(positions, normals, indices)
         .with_uv(uv)
@@ -224,11 +236,12 @@ pub fn add_floor_visuals(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     default_floor_vis: Query<(&GlobalFloorVisibility, &RecencyRanking<DrawingMarker>)>,
+    lifts: Query<(&Transform, &LiftCabin<Entity>)>,
 ) {
     for (e, new_floor, texture_source, rank, vis, parent) in &floors {
         let (base_color_texture, texture) = from_texture_source(texture_source, &textures);
 
-        let mesh = make_floor_mesh(e, new_floor, &texture, &anchors);
+        let mesh = make_floor_mesh(e, new_floor, &texture, &anchors, &lifts);
         let height = floor_height(rank);
         let default_vis = parent
             .map(|p| default_floor_vis.get(p.get()).ok())
@@ -296,13 +309,14 @@ pub fn update_floors_for_moved_anchors(
     >,
     mut mesh_assets: ResMut<Assets<Mesh>>,
     mut mesh_handles: Query<&mut Handle<Mesh>>,
+    lifts: Query<(&Transform, &LiftCabin<Entity>)>,
 ) {
     for dependents in &changed_anchors {
         for dependent in dependents.iter() {
             if let Some((e, segments, path, texture_source)) = floors.get(*dependent).ok() {
                 let (_, texture) = from_texture_source(texture_source, &textures);
                 if let Ok(mut mesh) = mesh_handles.get_mut(segments.mesh) {
-                    *mesh = mesh_assets.add(make_floor_mesh(e, path, &texture, &anchors));
+                    *mesh = mesh_assets.add(make_floor_mesh(e, path, &texture, &anchors, &lifts));
                 }
             }
         }
@@ -328,6 +342,7 @@ pub fn update_floors(
     material_handles: Query<&Handle<StandardMaterial>>,
     anchors: AnchorParams,
     textures: Query<(Option<&Handle<Image>>, &Texture)>,
+    lifts: Query<(&Transform, &LiftCabin<Entity>)>,
 ) {
     for e in changed_floors.iter().chain(
         changed_texture_sources
@@ -340,11 +355,38 @@ pub fn update_floors(
         let (base_color_texture, texture) = from_texture_source(texture_source, &textures);
         if let Ok(mut mesh) = mesh_handles.get_mut(segment.mesh) {
             if let Ok(material) = material_handles.get(segment.mesh) {
-                *mesh = meshes.add(make_floor_mesh(e, path, &texture, &anchors));
+                *mesh = meshes.add(make_floor_mesh(e, path, &texture, &anchors, &lifts));
                 if let Some(mut material) = materials.get_mut(material) {
                     material.base_color_texture = base_color_texture;
                 }
             }
+        }
+    }
+}
+
+pub fn update_floors_for_changed_lifts(
+    lifts: Query<(&Transform, &LiftCabin<Entity>)>,
+    changed_lifts: Query<
+        (),
+        Or<(
+            Changed<LiftCabin<Entity>>,
+            (With<LiftCabin<Entity>>, Changed<GlobalTransform>),
+        )>,
+    >,
+    removed_lifts: RemovedComponents<LiftCabin<Entity>>,
+    floors: Query<(Entity, &FloorSegments, &Path<Entity>, &Affiliation<Entity>), With<FloorMarker>>,
+    anchors: AnchorParams,
+    textures: Query<(Option<&Handle<Image>>, &Texture)>,
+    mut mesh_assets: ResMut<Assets<Mesh>>,
+    mut mesh_handles: Query<&mut Handle<Mesh>>,
+) {
+    if changed_lifts.is_empty() && removed_lifts.is_empty() {
+        return;
+    }
+    for (e, segments, path, texture_source) in floors.iter() {
+        let (_, texture) = from_texture_source(texture_source, &textures);
+        if let Ok(mut mesh) = mesh_handles.get_mut(segments.mesh) {
+            *mesh = mesh_assets.add(make_floor_mesh(e, path, &texture, &anchors, &lifts));
         }
     }
 }
