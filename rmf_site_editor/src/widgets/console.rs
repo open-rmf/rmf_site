be22ce19c27c28ab32058c6945f216073f3712e2
Xiyu Oh <xiyu@openrobotics.org>
2024-08-16 15:57:54 +0800

    Remove todo comment
    Signed-off-by: Xiyu Oh <xiyu@openrobotics.org>

--- rmf_site_editor/src/widgets/console.rs
+++ rmf_site_editor/src/widgets/console.rs
@@ -75,9 +75,6 @@ fn console_widget(In(input): In<PanelWidgetInput>, mut log_history: ResMut<LogHi
                             });
                         }
                         // Slider to adjust display limit
-                        // TODO(@mxgrey): Consider allowing this range to
-                        // automatically grow/shrink when the selected value
-                        // approaches or leaves the upper limit.
                         let history_size = log_history.log_history().len() as f64;
                         let nearest_hundred: usize = 100*(history_size/100.0).ceil() as usize;
                         ui.add(egui::Slider::new(
