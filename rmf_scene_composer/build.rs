1e0d73f0ca8a01f4835274148cc15325b5ebfb3a
Michael X. Grey <greyxmike@gmail.com>
2025-04-07 21:42:31 +0800

    Skeleton for scene generation
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- rmf_scene_composer/build.rs
+++ rmf_scene_composer/build.rs
@@ -15,4 +15,41 @@
  *
 */
 
-fn main() {}
+use std::path::PathBuf;
+use thiserror::Error as ThisError;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let gz_msgs_name = "gz-msgs11";
+    let libdir = pkg_config::get_variable(gz_msgs_name, "libdir")
+        .map_err(box_error)?;
+
+    let libdir_path = PathBuf::from(libdir);
+    let base_protos_path = libdir_path.parent()
+        .ok_or_else(|| box_error(MessageGenerationError::CannotFindShareDirectory))?
+        .join("share")
+        .join("gz")
+        .join(gz_msgs_name)
+        .join("protos");
+
+    let protos_path = base_protos_path
+        .join("gz")
+        .join("msgs");
+
+    prost_build::compile_protos(
+        &[protos_path.join("scene.proto")],
+        &[base_protos_path, protos_path],
+    )
+    .map_err(box_error)?;
+
+    Ok(())
+}
+
+#[derive(ThisError, Debug)]
+enum MessageGenerationError {
+    #[error("The share directory of gz-msgs was not in its expected location relative to its library location")]
+    CannotFindShareDirectory,
+}
+
+fn box_error<E: std::error::Error + 'static>(err: E) -> Box<dyn std::error::Error> {
+    Box::new(err)
+}
