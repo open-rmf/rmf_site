b7cb76afb584d67057f4bc6c2ef45a7eefe4a93b
rydb <ryan.dobe@gmail.com>
2025-07-07 15:01:34 -0500

    moved some widgets
    Signed-off-by: rydb <ryan.dobe@gmail.com>

--- crates/rmf_site_editor/src/widgets/mod.rs
+++ crates/rmf_site_editor/src/widgets/mod.rs
@@ -74,8 +74,8 @@ use diagnostics::*;
 pub mod fuel_asset_browser;
 pub use fuel_asset_browser::*;
 
-pub mod header_panel;
-pub use header_panel::*;
+// pub mod header_panel;
+// pub use header_panel::*;
 
 pub mod icons;
 pub use icons::*;
@@ -83,17 +83,17 @@ pub use icons::*;
 pub mod inspector;
 pub use inspector::*;
 
-pub mod menu_bar;
-pub use menu_bar::*;
+// pub mod menu_bar;
+// pub use menu_bar::*;
 
 pub mod move_layer;
 pub use move_layer::*;
 
-pub mod panel_of_tiles;
-pub use panel_of_tiles::*;
+// pub mod panel_of_tiles;
+// pub use panel_of_tiles::*;
 
-pub mod panel;
-pub use panel::*;
+// pub mod panel;
+// pub use panel::*;
 
 pub mod properties_panel;
 pub use properties_panel::*;
@@ -173,6 +173,7 @@ impl Plugin for StandardUiPlugin {
                 ConsoleWidgetPlugin::default(),
                 WorkspaceMenuPlugin::default(),
                 UserCameraDisplayPlugin::default(),
+                StandardCreationPlugin,
                 #[cfg(not(target_arch = "wasm32"))]
                 SdfExportMenuPlugin::default(),
                 #[cfg(not(target_arch = "wasm32"))]
@@ -192,223 +193,223 @@ impl Plugin for StandardUiPlugin {
     }
 }
 
-/// This component should be given to an entity that needs to be rendered as a
-/// nested widget in the UI.
-///
-/// For standard types of widgets you don't need to create this component yourself,
-/// instead use one of the generic convenience plugins:
-/// - [`InspectionPlugin`]
-/// - [`PropertiesTilePlugin`]
-#[derive(Component)]
-pub struct Widget<Input = (), Output = ()> {
-    inner: Option<Box<dyn ExecuteWidget<Input, Output> + 'static + Send + Sync>>,
-    _ignore: std::marker::PhantomData<(Input, Output)>,
-}
+// /// This component should be given to an entity that needs to be rendered as a
+// /// nested widget in the UI.
+// ///
+// /// For standard types of widgets you don't need to create this component yourself,
+// /// instead use one of the generic convenience plugins:
+// /// - [`InspectionPlugin`]
+// /// - [`PropertiesTilePlugin`]
+// #[derive(Component)]
+// pub struct Widget<Input = (), Output = ()> {
+//     inner: Option<Box<dyn ExecuteWidget<Input, Output> + 'static + Send + Sync>>,
+//     _ignore: std::marker::PhantomData<(Input, Output)>,
+// }
 
-impl<Input, Output> Widget<Input, Output>
-where
-    Input: 'static + Send + Sync,
-    Output: 'static + Send + Sync,
-{
-    pub fn new<W>(world: &mut World) -> Self
-    where
-        W: WidgetSystem<Input, Output> + 'static + Send + Sync,
-    {
-        let inner = InnerWidget::<Input, Output, W> {
-            state: SystemState::new(world),
-            _ignore: Default::default(),
-        };
+// impl<Input, Output> Widget<Input, Output>
+// where
+//     Input: 'static + Send + Sync,
+//     Output: 'static + Send + Sync,
+// {
+//     pub fn new<W>(world: &mut World) -> Self
+//     where
+//         W: WidgetSystem<Input, Output> + 'static + Send + Sync,
+//     {
+//         let inner = InnerWidget::<Input, Output, W> {
+//             state: SystemState::new(world),
+//             _ignore: Default::default(),
+//         };
 
-        Self {
-            inner: Some(Box::new(inner)),
-            _ignore: Default::default(),
-        }
-    }
-}
+//         Self {
+//             inner: Some(Box::new(inner)),
+//             _ignore: Default::default(),
+//         }
+//     }
+// }
 
-/// Do not implement this widget directly. Instead create a struct that derives
-/// [`SystemParam`] and then implement [`WidgetSystem`] for that struct.
-pub trait ExecuteWidget<Input, Output> {
-    fn show(&mut self, input: Input, ui: &mut Ui, world: &mut World) -> Output;
-}
+// /// Do not implement this widget directly. Instead create a struct that derives
+// /// [`SystemParam`] and then implement [`WidgetSystem`] for that struct.
+// pub trait ExecuteWidget<Input, Output> {
+//     fn show(&mut self, input: Input, ui: &mut Ui, world: &mut World) -> Output;
+// }
 
-/// Implement this on a [`SystemParam`] struct to make it a widget that can be
-/// plugged into the site editor UI.
-///
-/// See documentation of [`PropertiesTilePlugin`] or [`InspectionPlugin`] to see
-/// examples of using this.
-pub trait WidgetSystem<Input = (), Output = ()>: SystemParam {
-    fn show(input: Input, ui: &mut Ui, state: &mut SystemState<Self>, world: &mut World) -> Output;
-}
+// /// Implement this on a [`SystemParam`] struct to make it a widget that can be
+// /// plugged into the site editor UI.
+// ///
+// /// See documentation of [`PropertiesTilePlugin`] or [`InspectionPlugin`] to see
+// /// examples of using this.
+// pub trait WidgetSystem<Input = (), Output = ()>: SystemParam {
+//     fn show(input: Input, ui: &mut Ui, state: &mut SystemState<Self>, world: &mut World) -> Output;
+// }
 
-struct InnerWidget<Input, Output, W: WidgetSystem<Input, Output> + 'static> {
-    state: SystemState<W>,
-    _ignore: std::marker::PhantomData<(Input, Output)>,
-}
+// struct InnerWidget<Input, Output, W: WidgetSystem<Input, Output> + 'static> {
+//     state: SystemState<W>,
+//     _ignore: std::marker::PhantomData<(Input, Output)>,
+// }
 
-impl<Input, Output, W> ExecuteWidget<Input, Output> for InnerWidget<Input, Output, W>
-where
-    W: WidgetSystem<Input, Output>,
-{
-    fn show(&mut self, input: Input, ui: &mut Ui, world: &mut World) -> Output {
-        let u = W::show(input, ui, &mut self.state, world);
-        self.state.apply(world);
-        u
-    }
-}
+// impl<Input, Output, W> ExecuteWidget<Input, Output> for InnerWidget<Input, Output, W>
+// where
+//     W: WidgetSystem<Input, Output>,
+// {
+//     fn show(&mut self, input: Input, ui: &mut Ui, world: &mut World) -> Output {
+//         let u = W::show(input, ui, &mut self.state, world);
+//         self.state.apply(world);
+//         u
+//     }
+// }
 
-pub type ShowResult<T = ()> = Result<T, ShowError>;
+// pub type ShowResult<T = ()> = Result<T, ShowError>;
 
-/// Errors that can happen while attempting to show a widget.
-#[derive(Debug)]
-pub enum ShowError {
-    /// The entity whose widget you are trying to show is missing from the world
-    EntityMissing,
-    /// There is no [`Widget`] component for the entity
-    WidgetMissing,
-    /// The entity has a [`Widget`] component, but the widget is already in use,
-    /// which implies that we are trying to render the widget recursively, and
-    /// that is not supported due to soundness issues.
-    Recursion,
-}
+// /// Errors that can happen while attempting to show a widget.
+// #[derive(Debug)]
+// pub enum ShowError {
+//     /// The entity whose widget you are trying to show is missing from the world
+//     EntityMissing,
+//     /// There is no [`Widget`] component for the entity
+//     WidgetMissing,
+//     /// The entity has a [`Widget`] component, but the widget is already in use,
+//     /// which implies that we are trying to render the widget recursively, and
+//     /// that is not supported due to soundness issues.
+//     Recursion,
+// }
 
-/// Trait implemented on [`World`] to let it render child widgets. Note that
-/// this is not able to render widgets recursively, so you should make sure not
-/// to have circular dependencies in your widget structure.
-pub trait TryShowWidgetWorld {
-    /// Try to show a widget that has `()` for input and output belonging to the
-    /// specified entity.
-    fn try_show(&mut self, entity: Entity, ui: &mut Ui) -> ShowResult<()> {
-        self.try_show_out(entity, (), ui)
-    }
+// /// Trait implemented on [`World`] to let it render child widgets. Note that
+// /// this is not able to render widgets recursively, so you should make sure not
+// /// to have circular dependencies in your widget structure.
+// pub trait TryShowWidgetWorld {
+//     /// Try to show a widget that has `()` for input and output belonging to the
+//     /// specified entity.
+//     fn try_show(&mut self, entity: Entity, ui: &mut Ui) -> ShowResult<()> {
+//         self.try_show_out(entity, (), ui)
+//     }
 
-    /// Same as [`Self::try_show`] but takes an input that will be fed to the widget.
-    fn try_show_in<Input>(&mut self, entity: Entity, input: Input, ui: &mut Ui) -> ShowResult<()>
-    where
-        Input: 'static + Send + Sync,
-    {
-        self.try_show_out(entity, input, ui)
-    }
+//     /// Same as [`Self::try_show`] but takes an input that will be fed to the widget.
+//     fn try_show_in<Input>(&mut self, entity: Entity, input: Input, ui: &mut Ui) -> ShowResult<()>
+//     where
+//         Input: 'static + Send + Sync,
+//     {
+//         self.try_show_out(entity, input, ui)
+//     }
 
-    /// Same as [`Self::try_show`] but takes an input for the widget and provides
-    /// an output from the widget.
-    fn try_show_out<Output, Input>(
-        &mut self,
-        entity: Entity,
-        input: Input,
-        ui: &mut Ui,
-    ) -> ShowResult<Output>
-    where
-        Input: 'static + Send + Sync,
-        Output: 'static + Send + Sync;
-}
+//     /// Same as [`Self::try_show`] but takes an input for the widget and provides
+//     /// an output from the widget.
+//     fn try_show_out<Output, Input>(
+//         &mut self,
+//         entity: Entity,
+//         input: Input,
+//         ui: &mut Ui,
+//     ) -> ShowResult<Output>
+//     where
+//         Input: 'static + Send + Sync,
+//         Output: 'static + Send + Sync;
+// }
 
-impl TryShowWidgetWorld for World {
-    fn try_show_out<Output, Input>(
-        &mut self,
-        entity: Entity,
-        input: Input,
-        ui: &mut Ui,
-    ) -> ShowResult<Output>
-    where
-        Input: 'static + Send + Sync,
-        Output: 'static + Send + Sync,
-    {
-        let Ok(mut entity_mut) = self.get_entity_mut(entity) else {
-            return Err(ShowError::EntityMissing);
-        };
-        entity_mut.try_show_out(input, ui)
-    }
-}
+// impl TryShowWidgetWorld for World {
+//     fn try_show_out<Output, Input>(
+//         &mut self,
+//         entity: Entity,
+//         input: Input,
+//         ui: &mut Ui,
+//     ) -> ShowResult<Output>
+//     where
+//         Input: 'static + Send + Sync,
+//         Output: 'static + Send + Sync,
+//     {
+//         let Ok(mut entity_mut) = self.get_entity_mut(entity) else {
+//             return Err(ShowError::EntityMissing);
+//         };
+//         entity_mut.try_show_out(input, ui)
+//     }
+// }
 
-/// Same as [`TryShowWidgetWorld`] but is implemented for [`EntityWorldMut`] so
-/// you do not need to specify the target entity.
-pub trait TryShowWidgetEntity {
-    /// Try to show a widget that has `()` for input and output
-    fn try_show(&mut self, ui: &mut Ui) -> ShowResult<()> {
-        self.try_show_out((), ui)
-    }
+// /// Same as [`TryShowWidgetWorld`] but is implemented for [`EntityWorldMut`] so
+// /// you do not need to specify the target entity.
+// pub trait TryShowWidgetEntity {
+//     /// Try to show a widget that has `()` for input and output
+//     fn try_show(&mut self, ui: &mut Ui) -> ShowResult<()> {
+//         self.try_show_out((), ui)
+//     }
 
-    fn try_show_in<Input>(&mut self, input: Input, ui: &mut Ui) -> ShowResult<()>
-    where
-        Input: 'static + Send + Sync,
-    {
-        self.try_show_out(input, ui)
-    }
+//     fn try_show_in<Input>(&mut self, input: Input, ui: &mut Ui) -> ShowResult<()>
+//     where
+//         Input: 'static + Send + Sync,
+//     {
+//         self.try_show_out(input, ui)
+//     }
 
-    fn try_show_out<Output, Input>(&mut self, input: Input, ui: &mut Ui) -> ShowResult<Output>
-    where
-        Input: 'static + Send + Sync,
-        Output: 'static + Send + Sync;
-}
+//     fn try_show_out<Output, Input>(&mut self, input: Input, ui: &mut Ui) -> ShowResult<Output>
+//     where
+//         Input: 'static + Send + Sync,
+//         Output: 'static + Send + Sync;
+// }
 
-impl<'w> TryShowWidgetEntity for EntityWorldMut<'w> {
-    fn try_show_out<Output, Input>(&mut self, input: Input, ui: &mut Ui) -> ShowResult<Output>
-    where
-        Input: 'static + Send + Sync,
-        Output: 'static + Send + Sync,
-    {
-        let Some(mut widget) = self.get_mut::<Widget<Input, Output>>() else {
-            return Err(ShowError::WidgetMissing);
-        };
+// impl<'w> TryShowWidgetEntity for EntityWorldMut<'w> {
+//     fn try_show_out<Output, Input>(&mut self, input: Input, ui: &mut Ui) -> ShowResult<Output>
+//     where
+//         Input: 'static + Send + Sync,
+//         Output: 'static + Send + Sync,
+//     {
+//         let Some(mut widget) = self.get_mut::<Widget<Input, Output>>() else {
+//             return Err(ShowError::WidgetMissing);
+//         };
 
-        let Some(mut inner) = widget.inner.take() else {
-            return Err(ShowError::Recursion);
-        };
+//         let Some(mut inner) = widget.inner.take() else {
+//             return Err(ShowError::Recursion);
+//         };
 
-        let output = self.world_scope(|world| inner.show(input, ui, world));
+//         let output = self.world_scope(|world| inner.show(input, ui, world));
 
-        if let Some(mut widget) = self.get_mut::<Widget<Input, Output>>() {
-            widget.inner = Some(inner);
-        }
+//         if let Some(mut widget) = self.get_mut::<Widget<Input, Output>>() {
+//             widget.inner = Some(inner);
+//         }
 
-        Ok(output)
-    }
-}
+//         Ok(output)
+//     }
+// }
 
-/// This is a marker trait to indicate that the system state of a widget can be
-/// safely shared across multiple renders of the widget. For example, the system
-/// parameters do not use the [`Changed`] filter. It is the responsibility of
-/// the user to ensure that sharing this widget will not have any bad side
-/// effects.
-///
-/// [`ShareableWidget`]s can be used by the [`ShowSharedWidget`] trait which is
-/// implemented for the [`World`] struct.
-pub trait ShareableWidget {}
+// /// This is a marker trait to indicate that the system state of a widget can be
+// /// safely shared across multiple renders of the widget. For example, the system
+// /// parameters do not use the [`Changed`] filter. It is the responsibility of
+// /// the user to ensure that sharing this widget will not have any bad side
+// /// effects.
+// ///
+// /// [`ShareableWidget`]s can be used by the [`ShowSharedWidget`] trait which is
+// /// implemented for the [`World`] struct.
+// pub trait ShareableWidget {}
 
-/// A resource to store a widget so that it can be reused multiple times in one
-/// render pass.
-#[derive(Resource)]
-pub struct SharedWidget<W: SystemParam + ShareableWidget + 'static> {
-    state: SystemState<W>,
-}
+// /// A resource to store a widget so that it can be reused multiple times in one
+// /// render pass.
+// #[derive(Resource)]
+// pub struct SharedWidget<W: SystemParam + ShareableWidget + 'static> {
+//     state: SystemState<W>,
+// }
 
-/// This gives a convenient function for rendering a widget using a world.
-pub trait ShowSharedWidget {
-    fn show<W, Output, Input>(&mut self, input: Input, ui: &mut Ui) -> Output
-    where
-        W: ShareableWidget + WidgetSystem<Input, Output> + 'static;
-}
+// /// This gives a convenient function for rendering a widget using a world.
+// pub trait ShowSharedWidget {
+//     fn show<W, Output, Input>(&mut self, input: Input, ui: &mut Ui) -> Output
+//     where
+//         W: ShareableWidget + WidgetSystem<Input, Output> + 'static;
+// }
 
-impl ShowSharedWidget for World {
-    fn show<W, Output, Input>(&mut self, input: Input, ui: &mut Ui) -> Output
-    where
-        W: ShareableWidget + WidgetSystem<Input, Output> + 'static,
-    {
-        if !self.contains_resource::<SharedWidget<W>>() {
-            let widget = SharedWidget::<W> {
-                state: SystemState::new(self),
-            };
-            self.insert_resource(widget);
-        }
+// impl ShowSharedWidget for World {
+//     fn show<W, Output, Input>(&mut self, input: Input, ui: &mut Ui) -> Output
+//     where
+//         W: ShareableWidget + WidgetSystem<Input, Output> + 'static,
+//     {
+//         if !self.contains_resource::<SharedWidget<W>>() {
+//             let widget = SharedWidget::<W> {
+//                 state: SystemState::new(self),
+//             };
+//             self.insert_resource(widget);
+//         }
 
-        self.resource_scope::<SharedWidget<W>, Output>(|world, mut widget| {
-            let u = W::show(input, ui, &mut widget.state, world);
-            widget.state.apply(world);
-            u
-        })
-    }
-}
+//         self.resource_scope::<SharedWidget<W>, Output>(|world, mut widget| {
+//             let u = W::show(input, ui, &mut widget.state, world);
+//             widget.state.apply(world);
+//             u
+//         })
+//     }
+// }
 
 /// This set is for systems that impact rendering the UI using egui. The
 /// [`UserCameraDisplay`] resource waits until after this set is finished before
---
+++ crates/rmf_site_ui/src/menu_bar/mod.rs
@@ -0,0 +1,343 @@
+/*
+ * Copyright (C) 2023 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+use bevy_ecs::prelude::*;
+use bevy_egui::egui::{self, Button, Ui};
+use bevy_app::prelude::*;
+
+use crate::*;
+
+/// Add the standard menu bar to the application.
+#[derive(Default)]
+pub struct MenuBarPlugin {}
+
+impl Plugin for MenuBarPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((
+            HeaderPanelPlugin::default(),
+            MenuDropdownPlugin::default(),
+        ))
+        .add_event::<MenuEvent>()
+        .init_resource::<FileMenu>()
+        .init_resource::<ToolMenu>()
+        .init_resource::<ViewMenu>();
+    }
+}
+
+/// Add a widget housing all the menu dropdown options
+#[derive(Default)]
+pub struct MenuDropdownPlugin {}
+
+impl Plugin for MenuDropdownPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(HeaderTilePlugin::<MenuDropdowns>::new());
+    }
+}
+
+#[derive(SystemParam)]
+struct MenuDropdowns<'w, 's> {
+    menus: Query<'w, 's, (&'static Menu, Entity)>,
+    menu_items: Query<'w, 's, (&'static mut MenuItem, Has<MenuDisabled>)>,
+    extension_events: EventWriter<'w, MenuEvent>,
+    view_menu: Res<'w, ViewMenu>,
+    file_menu: Res<'w, FileMenu>,
+    children: Query<'w, 's, &'static Children>,
+    top_level_components: Query<'w, 's, (), Without<ChildOf>>,
+}
+
+impl<'w, 's> WidgetSystem<Tile> for MenuDropdowns<'w, 's> {
+    fn show(_: Tile, ui: &mut Ui, state: &mut SystemState<Self>, world: &mut World) {
+        let mut params = state.get_mut(world);
+        ui.menu_button("File", |ui| {
+            render_sub_menu(
+                ui,
+                &params.file_menu.get(),
+                &params.children,
+                &params.menus,
+                &params.menu_items,
+                &mut params.extension_events,
+                true,
+            );
+        });
+        ui.menu_button("View", |ui| {
+            render_sub_menu(
+                ui,
+                &params.view_menu.get(),
+                &params.children,
+                &params.menus,
+                &params.menu_items,
+                &mut params.extension_events,
+                true,
+            );
+        });
+
+        for (_, entity) in params.menus.iter().filter(|(_, entity)| {
+            params.top_level_components.contains(*entity)
+                && (*entity != params.file_menu.get() && *entity != params.view_menu.get())
+        }) {
+            render_sub_menu(
+                ui,
+                &entity,
+                &params.children,
+                &params.menus,
+                &params.menu_items,
+                &mut params.extension_events,
+                false,
+            );
+        }
+        ui.separator();
+    }
+}
+
+/// Adding this to an entity to an entity with the [`MenuItem`] component
+/// will grey out and disable a [`MenuItem`].
+#[derive(Component)]
+pub struct MenuDisabled;
+
+/// This component represents a menu. Menus and menu items
+/// can be arranged in trees using bevy's own parent-child system.
+#[derive(Component)]
+pub struct Menu {
+    text: String,
+}
+
+impl Menu {
+    /// Create a new menu from the title
+    pub fn from_title(text: String) -> Self {
+        Self { text }
+    }
+
+    /// Retrieve the menu name
+    fn get(&self) -> String {
+        self.text.clone()
+    }
+}
+
+/// Create a new menu item
+#[derive(Component)]
+#[non_exhaustive]
+pub enum MenuItem {
+    /// Text + Shortcut hint if available
+    Text(TextMenuItem),
+    CheckBox(String, bool),
+}
+
+impl MenuItem {
+    pub fn checkbox_value(&self) -> Option<bool> {
+        match self {
+            MenuItem::CheckBox(_, value) => Some(*value),
+            _ => None,
+        }
+    }
+
+    pub fn checkbox_value_mut(&mut self) -> Option<&mut bool> {
+        match self {
+            &mut MenuItem::CheckBox(_, ref mut value) => Some(value),
+            _ => None,
+        }
+    }
+}
+
+pub struct TextMenuItem {
+    pub text: String,
+    pub shortcut: Option<String>,
+}
+
+impl From<&str> for TextMenuItem {
+    fn from(text: &str) -> Self {
+        Self {
+            text: text.into(),
+            shortcut: None,
+        }
+    }
+}
+
+impl TextMenuItem {
+    pub fn new(text: &str) -> Self {
+        text.into()
+    }
+
+    pub fn shortcut(mut self, shortcut: &str) -> Self {
+        self.shortcut = Some(shortcut.into());
+        self
+    }
+}
+
+/// This resource provides the root entity for the file menu
+#[derive(Resource)]
+pub struct FileMenu {
+    /// Map of menu items
+    menu_item: Entity,
+}
+
+impl FileMenu {
+    pub fn get(&self) -> Entity {
+        return self.menu_item;
+    }
+}
+
+impl FromWorld for FileMenu {
+    fn from_world(world: &mut World) -> Self {
+        let menu_item = world
+            .spawn(Menu {
+                text: "File".to_string(),
+            })
+            .id();
+        Self { menu_item }
+    }
+}
+
+/// This resource provides the root entity for the tool menu
+#[derive(Resource)]
+pub struct ToolMenu {
+    /// Map of menu items
+    menu_item: Entity,
+}
+
+impl ToolMenu {
+    pub fn get(&self) -> Entity {
+        return self.menu_item;
+    }
+}
+
+impl FromWorld for ToolMenu {
+    fn from_world(world: &mut World) -> Self {
+        let menu_item = world
+            .spawn(Menu {
+                text: "Tool".to_string(),
+            })
+            .id();
+        Self { menu_item }
+    }
+}
+
+/// This resource provides the root entity for the tool menu
+#[derive(Resource)]
+pub struct ViewMenu {
+    /// Map of menu items
+    menu_item: Entity,
+}
+
+impl ViewMenu {
+    pub fn get(&self) -> Entity {
+        return self.menu_item;
+    }
+}
+
+impl FromWorld for ViewMenu {
+    fn from_world(world: &mut World) -> Self {
+        let menu_item = world
+            .spawn(Menu {
+                text: "View".to_string(),
+            })
+            .id();
+        Self { menu_item }
+    }
+}
+
+#[non_exhaustive]
+#[derive(Event)]
+pub enum MenuEvent {
+    MenuClickEvent(Entity),
+}
+
+impl MenuEvent {
+    pub fn clicked(&self) -> bool {
+        matches!(self, Self::MenuClickEvent(_))
+    }
+
+    pub fn source(&self) -> Entity {
+        match self {
+            Self::MenuClickEvent(entity) => *entity,
+        }
+    }
+}
+
+/// Helper function to render a submenu starting at the entity.
+pub fn render_sub_menu(
+    ui: &mut Ui,
+    entity: &Entity,
+    children: &Query<&Children>,
+    menus: &Query<(&Menu, Entity)>,
+    menu_items: &Query<(&mut MenuItem, Has<MenuDisabled>)>,
+    extension_events: &mut EventWriter<MenuEvent>,
+    skip_top_label: bool,
+) {
+    if let Ok((e, disabled)) = menu_items.get(*entity) {
+        // Draw ui
+        match e {
+            MenuItem::Text(item) => {
+                let mut button = Button::new(&item.text);
+                if let Some(shortcut) = &item.shortcut {
+                    button = button.shortcut_text(shortcut);
+                }
+                if ui.add_enabled(!disabled, button).clicked() {
+                    extension_events.write(MenuEvent::MenuClickEvent(*entity));
+                }
+            },
+            &MenuItem::CheckBox(ref title, mut value) => {
+                if ui
+                    .add_enabled(!disabled, egui::Checkbox::new(&mut value, title))
+                    .clicked()
+                {
+                    extension_events.write(MenuEvent::MenuClickEvent(*entity));
+                }
+            },
+        }
+        return;
+    }
+
+    let Ok((menu, _)) = menus.get(*entity) else {
+        return;
+    };
+
+    if !skip_top_label {
+        ui.menu_button(&menu.get(), |ui| {
+            let Ok(child_items) = children.get(*entity) else {
+                return;
+            };
+
+            for child in child_items.iter() {
+                render_sub_menu(
+                    ui,
+                    &child,
+                    children,
+                    menus,
+                    menu_items,
+                    extension_events,
+                    false,
+                );
+            }
+        });
+    } else {
+        let Ok(child_items) = children.get(*entity) else {
+            return;
+        };
+
+        for child in child_items.iter() {
+            render_sub_menu(
+                ui,
+                &child,
+                children,
+                menus,
+                menu_items,
+                extension_events,
+                false,
+            );
+        }
+    }
+}
