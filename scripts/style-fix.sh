efe4f56e5688606869f2495d6fa75af0ae8bcd01
Michael X. Grey <greyxmike@gmail.com>
2025-10-25 02:18:30 +0800

    Update style check to edition 2024
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- scripts/style-fix.sh
+++ scripts/style-fix.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rustfmt --edition 2021 rmf_site_format/src/lib.rs rmf_site_editor/src/lib.rs rmf_site_editor/src/main.rs
+rustfmt --edition 2024 rmf_site_format/src/lib.rs rmf_site_editor/src/lib.rs rmf_site_editor/src/main.rs
